350ec648a401b4c6adf9a0c62b74eaca97347318
yuki_yano <yuki.ycino@gmail.com>
2022-01-25 00:35:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# coc-tsnip
+
+## Install
+
+`:CocInstall coc-tsnip`
+
+## License
+
+MIT
+
+---
+
+> This extension is built with [create-coc-extension](https://github.com/fannheyward/create-coc-extension)
---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import { CompleteResult, events, ExtensionContext, sources, VimCompleteItem, workspace } from 'coc.nvim';
+
+export const activate = async (context: ExtensionContext): Promise<void> => {
+  context.subscriptions.push(
+    sources.createSource({
+      name: 'tsnip source',
+      doComplete: async (option) => {
+        if (option.input === '') {
+          return {
+            items: [],
+          };
+        }
+
+        const items = await getCompletionItems();
+        return items;
+      },
+    })
+  );
+
+  events.on('CompleteDone', async (item: VimCompleteItem) => {
+    if (item.menu !== '[tsnip]') {
+      return;
+    }
+
+    await workspace.nvim.call('tsnip#remove_suffix_word', [item.word]);
+    await workspace.nvim.command(`TSnip ${item.word}`);
+  });
+};
+
+const getCompletionItems = async (): Promise<CompleteResult> => {
+  const snippets = (await workspace.nvim.call('tsnip#items')) as Array<{ word: string; info: string }>;
+
+  return {
+    items: snippets.map(({ word, info }) => {
+      return {
+        word,
+        info,
+        menu: '[tsnip]',
+        dup: 1,
+      };
+    }),
+    priority: 1000,
+  };
+};
